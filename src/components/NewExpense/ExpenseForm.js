23b54a017a218b3b8ccb084b6d69ef2c8c7ff689
Diwas123456 <diwasadhikari92@gmail.com>
2022-07-29 13:13:32 +0545

    make changes to files

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -71,7 +71,7 @@ const ExpenseForm = (props) => {
                     <input value={enteredDate} type='date'  min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}></input>
                 </div>
 
-
+                    
             </div>
 
             <div className='new-expense__actions'>
